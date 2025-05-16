ca714df2630d46f50a3bde0645a124590060ab6c
yasminefourti <fourtiyasmine26@gmail.com>
2025-05-16 09:45:10 +0200

    etape5 :Gérer la case à cocher

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 
-function TodoItem({tache ,onToggle, onDelete}) {
-
-    const itemStyle = {
-        textDecoration: tache.done ? 'line-through' : 'none',
-        color: tache.done ? 'gray' : 'black',
-        display: 'flex',
-        alignItems: 'center',
-        gap: '10px',
-        marginBottom: '8px'
-    };
+function TodoItem({ tache, onToggle, onDelete }) {
+  const itemStyle = {
+    textDecoration: tache.done ? 'line-through' : 'none',
+    color: tache.done ? 'gray' : 'black',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+    marginBottom: '8px',
+    padding: '10px',
+    border: '1px solid #ddd',
+    borderRadius: '6px',
+    justifyContent: 'space-between'
+  };
 
   return (
-    
-    
-    <li>
-        <input type="checkbox" checked={tache.done}  onChange={() => onToggle(tache.id)} />
-        <span>{tache.title}</span>   
-        <button onClick={() => alert("modifier tache")}  >Modifier</button>
+    <li style={itemStyle}>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+        <input
+          type="checkbox"
+          checked={tache.done}
+          onChange={() => onToggle(tache.id)}
+        />
+        <span>{tache.title}</span>
+      </div>
+      <div style={{ display: 'flex', gap: '8px' }}>
+        <button onClick={() => alert("modifier tache")}>Modifier</button>
         <button onClick={() => onDelete(tache.id)}>Supprimer</button>
+      </div>
     </li>
-    
   );
 }
 
